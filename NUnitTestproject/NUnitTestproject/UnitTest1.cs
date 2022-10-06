795c134113b6d5e7ec690c843bb6f77ffbdaf1d8
claudia.maraciuc@evozon.com <claudia.maraciuc@evozon.onmicrosoft.com>
2022-10-06 15:41:20 +0300

    add project + gitignore

---
+++ NUnitTestproject/NUnitTestproject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NUnitTestproject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
