8d7b310c88438468b97392c7f25f1f8a4dd34cb8
claudia.maraciuc@evozon.com <claudia.maraciuc@evozon.onmicrosoft.com>
2022-10-27 17:44:09 +0300

    add 2 tests

---
+++ AutomationTesting/AutomationTesting/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTesting
+{
+     class Class1
+    {
+        public void Test1(string message)
+        {
+            Console.WriteLine(message); 
+        }
+        public void Test2(string message)
+        { Console.WriteLine(message);}
+    }
+}
--- AutomationTesting/AutomationTesting/Program.cs
+++ AutomationTesting/AutomationTesting/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AutomationTesting;
+
 Console.WriteLine("Hello, World!");
+
+
+Class1 myFirstClass = new Class1();
+myFirstClass.Test1("This is my first test");
+myFirstClass.Test2("This is myFirstClass second test");
