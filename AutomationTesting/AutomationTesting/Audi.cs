480469dbeb8e7bd8a0109fd4370611e4dcaf2de6
claudia.maraciuc@evozon.com <claudia.maraciuc@evozon.onmicrosoft.com>
2022-10-27 18:32:11 +0300

    explicit casting

--- AutomationTesting/AutomationTesting/Audi.cs
+++ AutomationTesting/AutomationTesting/Audi.cs
@@ -8,6 +8,10 @@ namespace AutomationTesting
 {
      class Audi 
     {
-
+        public string Name { get; set; }    
+        public void Warning()
+        {
+            Console.WriteLine("Error");
+        }
     }
 }
--- AutomationTesting/AutomationTesting/BMW.cs
+++ AutomationTesting/AutomationTesting/BMW.cs
@@ -8,6 +8,10 @@ namespace AutomationTesting
 {
      class BMW 
     {
-
+        public string Color { get; set; }
+        public void Warning()
+        {
+            Console.WriteLine("Alert");
+        }
     }
 }
