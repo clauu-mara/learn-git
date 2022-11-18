49fa2d4a34d5329de216d546fda44fc85b018557
Maraciuc Claudia <80710033+clauu-mara@users.noreply.github.com>
2022-11-18 11:25:20 +0200

    refactoring (#22)
    Co-authored-by: claudia.maraciuc@evozon.com <claudia.maraciuc@evozon.onmicrosoft.com>

--- AutomationTesting/AutomationTesting/Models/Car.cs
+++ AutomationTesting/AutomationTesting/Models/Car.cs
@@ -2,26 +2,20 @@
 {
     public abstract class Car
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int Price { get; set; } = 4;
-        public string Model { get; set; }
+        public string? Model { get; set; }
 
         public void CheckPrice()
         {
-            //TODO refactoring
             if (Price == 0)
-            {
                 Console.WriteLine("No price");
-            }
             else
-            {
                 Console.WriteLine("This is the price" + Price);
-            }
         }
 
         public string CheckPrice2()
         {
-            //TODO refactoring
             return Price == 0 ? "No price" : "Price exists";
         }
 
--- AutomationTesting/AutomationTesting/Models/Class2.cs
+++ AutomationTesting/AutomationTesting/Models/Class2.cs
@@ -4,27 +4,16 @@
     {
         private int _myVariable;
 
-        //TODO refactoring
-        public void SetValue(int value)
-        {
-            _myVariable = value;
-        }
+        public void SetValue(int value) =>  _myVariable = value;
 
         public void GetValue()
         {
-            //TODO refactoring
             if (_myVariable > 0)
-            {
                 Console.WriteLine("Variable {0} > 0 ", _myVariable);
-            }
             else if (_myVariable < 0)
-            {
                 Console.WriteLine("Variable {0} < 0 ", _myVariable);
-            }
             else
-            {
                 Console.WriteLine("Variable = 0");
-            }
         }
     }
 }
